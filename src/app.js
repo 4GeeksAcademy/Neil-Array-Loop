705bcde6353d0a1c02014623d88d9529d7f13d9e
Neil-Player456 <Nmrubix11@gmail.com>
2025-02-22 00:15:02 +0000

    Fin

--- src/app.js
+++ src/app.js
@@ -5,7 +5,17 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = function () {
+  let first = ["Dog", "Cat", "Bird", "Rat", "Fish"];
+  let second = ["Grass", "Dirt", "Water", "Mud", "Rocks"];
+  let third = [".com", ".org", ".to", ".net", ".co"]
+
+
+  for (let i = 0; i < first.length; i++) {
+    for (let z = 0; z < second.length; z++) {
+      for (let y = 0; y < third.length; y++) {
+        console.log(first[i], second[z], third[y])
+      }
+    }
+  }
 };
